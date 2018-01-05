c737ea2dbf5e4e5759848d8c2ef9e55ac7efc177
kvaps <kvapss@gmail.com>
2018-01-05 12:57:49 +0100

    fix: status check output

--- dockerfiles/drbd/drbd-wrapper.sh
+++ dockerfiles/drbd/drbd-wrapper.sh
@@ -37,7 +37,7 @@ fi
 $MODPROBE drbd
 
 # Stopping resource if it is already exists
-if $DRBDADM status lustre1-mdt0 2>/dev/null; then
+if $DRBDADM status lustre1-mdt0 &>/dev/null; then
     $DRBDADM down "$RESOURCE_NAME"
 fi
 
